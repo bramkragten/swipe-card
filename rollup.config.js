5bd7b71dad3cf8e93fc66de6e0bff34c65179474
Bram Kragten <mail@bramkragten.nl>
2020-03-19 20:35:09 +0100

    bump

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import postcss from "rollup-plugin-postcss";
 import babel from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser";
