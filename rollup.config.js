7cf9a18f7439e0e7304f0ad720fe1525a9d62735
Bram Kragten <mail@bramkragten.nl>
2020-06-14 18:18:15 +0200

    Bump versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from "@rollup/plugin-node-resolve";
 import postcss from "rollup-plugin-postcss";
-import babel from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import cleanup from "rollup-plugin-cleanup";
 
@@ -14,10 +13,6 @@ export default {
     resolve(),
     postcss({
       extensions: [".css"],
-    }),
-    babel({
-      exclude: "node_modules/**",
-      babelrc: false,
     }),
     terser(),
     cleanup({ comments: "none" }),
